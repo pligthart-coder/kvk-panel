8d2e5e19928cd7f7295234debe10ac17bfc9e6e2
Patrick Ligthart <Patrick@mac.home>
2026-05-07 15:16:03 +0200

    Add country mapping: toVisitCountryNode

--- lib/carerix.ts
+++ lib/carerix.ts
@@ -165,9 +165,16 @@ export async function createCompanyInCarerix(kvkCompany: any): Promise<{ success
       visitPostalCode: kvkCompany.address.postalCode || null,
       visitCity: kvkCompany.address.city || null,
       url: kvkCompany.website || null,
+      toVisitCountryNode: kvkCompany.address.country ? {
+        _kind: 'CRDataNode',
+        _lookup: {
+          key: 'value',
+          value: kvkCompany.address.country
+        }
+      } : null,
       // Note: Carerix uses "visit" prefix for address fields
       // Most KVK fields are NOT supported in the standard schema:
-      // - country, tradeNames, legalForm, establishmentNumber, rsin
+      // - tradeNames, legalForm, establishmentNumber, rsin
       // - isActive, sbiCodes, registeredAt, employeeCount, branchCount
       // You need to add these as CUSTOM FIELDS in Carerix to store this data
     },
